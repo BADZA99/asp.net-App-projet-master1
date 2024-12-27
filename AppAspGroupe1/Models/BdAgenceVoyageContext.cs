acb64dfdf1f92e4d489806999a6263a2b2581365
papabn <pndiaye999@gmail.com>
2024-12-27 02:04:43 +0000

    first commit

---
+++ AppAspGroupe1/Models/BdAgenceVoyageContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace AppAspGroupe1.Models
+{
+    public class BdAgenceVoyageContext:DbContext
+    {
+        public BdAgenceVoyageContext():base("ConnAgenceVoyage")
+        {
+
+        }
+
+        public DbSet <Chauffeur> Chauffeurs { get; set; }
+    }
+}
